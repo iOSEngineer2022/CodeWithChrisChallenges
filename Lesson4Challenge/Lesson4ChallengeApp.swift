d86d2e69d0e5e57d83a5c8c7286348b6e6ea0620
Yüksel Toprak <yuekseltoprak@mailbox.org>
2022-06-14 15:09:46 +0200

    Initial Commit

---
+++ Lesson4Challenge/Lesson4ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson4ChallengeApp.swift
+//  Lesson4Challenge
+//
+//  Created by Yüksel Toprak on 14.06.22.
+//
+
+import SwiftUI
+
+@main
+struct Lesson4ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
